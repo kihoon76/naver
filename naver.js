864fdc8080c5e542df3f51b0614193bc45cbfd5c
khnam <lovedeer118@gmail.com>
2017-09-11 11:55:55 +0900

    naver

---
+++ naver.js
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+ var express   	= require('express'),
+ 	 fs		    = require('fs'),
+ 	 handlebars	= require('express3-handlebars'),
+ 	 bodyParser	= require('body-parser'),
+ 	 path		= require('path'),
+ 	 log4js		= require('log4js');
+
+ log4js.configure({
+	appenders: {naver: {type:'console'}},
+	categories: {default: {appenders: ['naver'], level: 'debug'}}
+ });
+
+ var app 		= express(),
+	viewsPath	= path.join(__dirname, 'views'),
+ 	logger		= log4js.getLogger('naver');
+ 	 
+ app.engine('html', handlebars({
+ 	defaultLayout : 'main',
+ 	extname : 'html',
+ 	layoutsDir: viewsPath + '/layouts'
+ })); 
+ 
+ app.set('view engine', 'html');
+ app.set('views', viewsPath);
+ app.set('port', process.env.NAVER_PORT || 10000);
+ app.disable('x-powered-by');
+ app.disable('etag');
+ 
+ app.use(express.static(path.join(__dirname, 'public')));
+ app.use(bodyParser.urlencoded({ extended: true}));
+ app.use('/', require('./routes/index'));
+ 
+ app.use(function(err, req, res, next) {
+ 	res.status(500).render('error');
+ });
+ 
+ app.use(function(err, req, res, next) {
+ 	res.status(404).render('not-found');
+ });
+ 
+ app.listen(app.get('port'), function() {
+ 	logger.debug('Listening on port ' + app.get('port'));
+ });
+ 
---
+++ routes/index.js
@@ -0,0 +1,59 @@
+var express  = require('express'),
+	request	 = require('request'),
+	log4js	 = require('log4js');
+
+var router   = express.Router();
+var logger		= log4js.getLogger('index.js')
+
+var options  = function(param) {
+	return {
+		url: 'https://openapi.naver.com/v1/map/reversegeocode.xml?encoding=utf-8&coordType=latlng&query=' + param.lat + ',' + param.lng,
+		method: 'GET',
+		headers : {
+			'X-Naver-Client-Id': param.key,
+			'X-Naver-Client-Secret': param.secret
+		}
+	}
+};
+
+router.use(function preProcess(req, res, next) {
+	logger.debug('요청 URL : ', req.originalUrl);
+	logger.debug('요청 METHOD : ', req.method);
+	
+	if(req.method == 'GET') {
+		logger.debug('파라미터  query : ', req.query);
+	}
+	else {
+		logger.debug('파라미터 params: ', req.params);
+	}
+	
+    next();
+});
+
+router.get('/api/naverform', function(req, res) {
+	res.render('naverform', {title:'네이버 전송폼'});
+});
+
+router.get('/api/naver', function(req, res) {
+	var key = req.query.key,
+		secret = req.query.secret
+		lat	= req.query.lat,
+		lng = req.query.lng;
+	
+	logger.debug('naver request');
+	request(options({key:key, secret:secret, lat:lat, lng:lng}), function(error, response, body) {
+		
+		if(!error && response.statusCode == 200) {
+			var info = res.send(body);
+			res.send(info);
+		}
+		else {
+			res.send(error);
+		}
+		
+		logger.debug('naver response');
+	});	
+		
+});
+
+module.exports = router;
